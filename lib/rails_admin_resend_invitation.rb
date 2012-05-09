f377a9386be89e9d9ccae3da8b49b04fb9109dbc
Marco V. Aguilar Soto <aguilarsoto@gmail.com>
2012-05-09 15:14:20 -0600

    do not show link if user has already confirmed their invitation

--- lib/rails_admin_resend_invitation.rb
+++ lib/rails_admin_resend_invitation.rb
@@ -16,7 +16,7 @@ module RailsAdmin
         end
 
         register_instance_option :visible? do
-          bindings[:abstract_model].model_name == "User"
+          bindings[:abstract_model].model_name == "User" &&  !bindings[:object].confirmed?
         end
 
         register_instance_option :controller do
